eea75bfd08594b69d187252447a27fcfbd74cda4
Vinicius Santana <vinicius.santanna@hotmail.com>
2018-11-28 00:15:40 -0300

    Criação de Componentes de Produto, Auth e roteamento

--- CervejariaZe-Angular/src/app/app.module.ts
+++ CervejariaZe-Angular/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ProdutoModule } from './produto/produto.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProdutoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
